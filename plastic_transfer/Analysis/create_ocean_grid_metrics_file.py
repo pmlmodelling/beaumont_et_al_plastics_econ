4587b1db756b5f13f7b037ddab4b8fd0bc8a956f
James Clark <jcl@pml.ac.uk>
2023-09-13 21:04:01 +0100

    Make grid_metrics dir if required

--- plastic_transfer/Analysis/create_ocean_grid_metrics_file.py
+++ plastic_transfer/Analysis/create_ocean_grid_metrics_file.py
@@ -9,6 +9,8 @@ The grid metrics file is created in the Inputs/grid_metrics directory.
 
 CMEMS data DOI: 10.48670/moi-00021
 """
+import pathlib
+
 from pylag.grid_metrics import create_arakawa_a_grid_metrics_file as create
 
 from project_paths import cmems_data_dir
@@ -16,7 +18,10 @@ from project_paths import cmems_data_dir
 
 in_file = f'{cmems_data_dir}/DAILY_GLOBAL_REANALYSIS_PHY_001_030-TDS_uv_2015_01.nc'
 
-out_file = f'../Inputs/grid_metrics/grid_metrics_surface_ocean.nc'
+# Create output directory
+pathlib.Path('../Inputs/grid_metrics').mkdir(parents=True, exist_ok=True)
 
+# Create grid metrics file
+out_file = f'../Inputs/grid_metrics/grid_metrics_surface_ocean.nc'
 create(in_file, is_global=True, surface_only=True, reference_var_name='uo',
        prng_seed=162, grid_metrics_file_name=out_file)
